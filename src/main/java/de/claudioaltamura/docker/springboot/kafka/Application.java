ff632ba87625f4279fbea19a4440d6633cebfc03
fred.jabbari <fred.jabbari@capturerx.com>
2019-01-26 22:20:44 -0600

    fred01

--- src/main/java/de/claudioaltamura/docker/springboot/kafka/Application.java
+++ src/main/java/de/claudioaltamura/docker/springboot/kafka/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+  /* a */
+  /* aa */
+  /* aaa */
+  /* aaaa */
+  /* aaaaa */
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
